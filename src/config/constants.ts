f6fadc78a6ea7fcb532c2ac5e26e1ae3eebce735
Chay Carnell <chaycarnell@gmail.com>
2025-07-17 13:28:45 +1000

    feat: add configs

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,5 @@
-// Global configuration constants
 export const BASE_PATH = "/bowendrivingschool";
 
-// Page routes enum using "as const" pattern
 export const ROUTES = {
   HOME: "/",
   ABOUT: "/about",
